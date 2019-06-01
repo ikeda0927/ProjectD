54c9f89dfeb0a066fa46afda4cdff8bc7addd58c
ikeda0927 <ikeda.kohei4477@gmail.com>
2019-06-01 19:20:23 +0900

    Rigidbody(AddExplosionForceとMovePositionについて)

---
+++ TestRigidbody/TestRigidbodyAssets/Resources/Rigidbody/TR_Explosion.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TR_Explosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    bool keyjudge = true;
+    public float force = 100f;
+    public float range = 1f;
+    private int DESTROY_TIME = 100;
+    private bool destroyJudge = false;
+    private int destoryCounter = 0;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (keyjudge)
+            {
+                var others = Physics.OverlapSphere(gameObject.transform.position, range);
+                foreach (Collider other in others)
+                {
+                    if (other.tag == "Block" || other.tag == "Bomb")
+                    {
+                        other.GetComponent<Rigidbody>().AddExplosionForce(force, gameObject.transform.position, range);
+                    }
+                }
+                //Destroy(gameObject);
+                destroyJudge = true;
+            }
+        }
+        else
+        {
+            keyjudge = true;
+        }
+        if (destroyJudge)
+        {
+            if (destoryCounter < DESTROY_TIME)
+            {
+                destoryCounter++;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ TestRigidbody/TestRigidbodyAssets/Resources/Rigidbody/TR_MovePosition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TR_MovePosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    const float force = 10f;
+    public float weight = .5f;
+    bool keyjudge = true;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        //rb.MovePosition(rb.position + Vector3.forward * force);
+        //rb.MovePosition(new Vector3(18f, 1f, 9f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (keyjudge)
+            {
+                keyjudge = false;
+                rb.MovePosition(new Vector3(rb.position.x + weight, rb.position.y, rb.position.z));
+            }
+        }
+        else
+        {
+            keyjudge = true;
+        }
+    }
+}
