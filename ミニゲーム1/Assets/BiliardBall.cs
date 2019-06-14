8576472c458c16cd05da155f3f5f3c1ab78b9d12
ikeda0927 <ikeda.kohei4477@gmail.com>
2019-06-15 00:24:41 +0900

    音の抑制

--- ミニゲーム1/Assets/BiliardBall.cs
+++ ミニゲーム1/Assets/BiliardBall.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class BiliardBall : MonoBehaviour
 {
     private AudioSource sound01;
+    private bool startJudge = false;
+    private const int WEIGHT = 60;
+    private int counter = 0;
     // Start is called before the first frame update
     void Start()
     {
         sound01 = GetComponent<AudioSource>();
+        //startJudge = true;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag == "1" || collision.transform.tag == "2" || collision.transform.tag == "3" || collision.transform.tag == "4" || collision.transform.tag == "5" || collision.transform.tag == "6" || collision.transform.tag == "7" || collision.transform.tag == "8" || collision.transform.tag == "9")
+        if (startJudge && (collision.transform.tag == "1" || collision.transform.tag == "2" || collision.transform.tag == "3" || collision.transform.tag == "4" || collision.transform.tag == "5" || collision.transform.tag == "6" || collision.transform.tag == "7" || collision.transform.tag == "8" || collision.transform.tag == "9"))
         {
             Debug.Log("Collision in BilliardBall : " + collision.transform.tag);
             sound01.PlayOneShot(sound01.clip);
@@ -22,6 +26,16 @@ public class BiliardBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (!startJudge)
+        {
+            if (counter < WEIGHT)
+            {
+                counter++;
+            }
+            else
+            {
+                startJudge = true;
+            }
+        }
     }
 }
