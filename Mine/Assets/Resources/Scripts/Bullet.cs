508a53dbcca3cecdab09106a26f3916dda79598f
Akira Yamasaki <yamayamaakira@gmail.com>
2019-06-28 15:50:51 +0900

    Mineを追加

---
+++ Mine/Assets/Resources/Scripts/Bullet.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float force = 100f;//爆発の威力
+    public float range = 5f;//爆発の範囲
+    GameObject effect;
+    ParticleSystem particle;
+    bool particleJudge;
+    bool collisionWithTarget;
+    void Start()
+    {
+        effect = gameObject.transform.Find("SmallExplosionEffect").gameObject;
+        particle = effect.GetComponent<ParticleSystem>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Target")
+        {
+            collisionWithTarget = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((gameObject != null && !particleJudge && gameObject.transform.position.y <= .6f) || collisionWithTarget)
+        {
+            Debug.Log("Explosion");
+            Explosion();
+        }
+        else if (gameObject != null && particleJudge && !particle.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+    void Explosion()
+    {
+        particleJudge = true;
+        effect.SetActive(true);
+        gameObject.GetComponent<SphereCollider>().enabled = false;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        //Bomb周辺(rangeの範囲)のオブジェクトを取得
+        var others = Physics.OverlapSphere(gameObject.transform.position, range);
+        foreach (Collider other in others)
+        {
+            //取得したオブジェクトのうち、タグが"Bomb"か"Block"だった場合、それらに爆発による力を加える
+            if (other.tag == "Bullet" || other.tag == "Target" || other.tag == "Tank")
+            {
+                //第二引数は爆発の中心地点(この場合はBombの中心)
+                other.GetComponent<Rigidbody>().AddExplosionForce(force, gameObject.transform.position, range);
+            }
+        }
+    }
+}
---
+++ Mine/Assets/Resources/Scripts/PlaySound.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    AudioSource sound01;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound01 = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space) == true || Input.GetKeyDown(KeyCode.B) == true || Input.GetKeyDown(KeyCode.V) == true || Input.GetKeyDown(KeyCode.N) == true)
+        //{
+        //    if (!sound01.enabled)
+        //    {
+        //        sound01.enabled = true;
+        //    }
+        //    Debug.Log("Sound true");
+        //    sound01.Play();
+        //}
+        //else
+        //{
+        //    Debug.Log("Sound false");
+        //    sound01.enabled = false;
+        //}
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S))
+        {
+            if (!sound01.enabled)
+            {
+                sound01.enabled = true;
+            }
+            if (!sound01.isPlaying)
+            {
+                Debug.Log("Sound true");
+                sound01.Play();
+            }
+        }
+        else
+        {
+            Debug.Log("Sound false");
+            sound01.enabled = false;
+        }
+        //else if (Input.GetKeyDown(KeyCode.Space) == false && Input.GetKeyDown(KeyCode.B) == false && Input.GetKeyDown(KeyCode.V) == false && Input.GetKeyDown(KeyCode.N) == false)
+        //{
+        //    //sound01.Stop();
+        //}
+        /*else if(Input.GetKeyDown(KeyCode.Space) == false)
+        {
+            sound01.Pause();
+        }*/
+    }
+}
